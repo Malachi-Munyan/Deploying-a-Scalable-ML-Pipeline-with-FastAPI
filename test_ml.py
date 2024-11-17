2b7263e6e0af7f7efa2683095c3ce96560973d85
Malachi-Munyan <mmuny10@wgu.edu>
2024-11-16 22:37:16 -0700

    Unit test written and ran

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,42 @@
 import pytest
-# TODO: add necessary import
+import numpy as numpy
+import pandas as pd
+from ml.model import train_model, compute_model_metrics
+from ml.data import process_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_column_count():
     """
-    # add description for the first test
+    Test to verify 15 columns in the dataset
     """
-    # Your code here
-    pass
+    df = pd.read_csv("data/census.csv")
+    column_count = 15
+
+    assert len(df.columns) == column_count
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_model_algorithm():
     """
-    # add description for the second test
+    Test to confirm the usage of Random Forest Classifier
     """
-    # Your code here
-    pass
+    X_holder = pd.DataFrame({'feature_1': [1, 2, 3], 'feature_2': ['A', 'B', 'C']})
+    y_holder = pd.Series([0,1,0])
+    X_holder_encoded = pd.get_dummies(X_holder)
+    model = train_model(X_holder_encoded, y_holder)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_metrics():
     """
-    # add description for the third test
+    Test the compute_model_metrics are operating within expected values
     """
-    # Your code here
-    pass
+    y_true = pd.Series([0, 1, 0, 1])
+    y_pred = pd.Series([0, 0, 1, 1])
+    precision, recall, f1 = compute_model_metrics(y_true, y_pred)
+
+    assert precision == 0.5
+    assert recall == 0.5
+    assert f1 == 0.5
